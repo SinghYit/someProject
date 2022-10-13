009cf4708e740059513dacef7ac6f0ce39a76bcb
Yit Singh <Yitsingh751@hotmail.com>
2022-10-13 10:45:38 +0200

    first commit

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', ()=> {
     navbarLinks.classList.toggle('active')
 })
+
+let openMenu = false;
+toggleButton.addEventListener('click', ()=> {
+    if(!openMenu) { 
+        toggleButton.classList.add('open');
+        openMenu = true;
+    }
+    else {
+        toggleButton.classList.remove('open');
+        openMenu = false
+    }
+})
